ec576c591b074e8733b7fa5b8646846623dff5b9
dregolanescu <dragos.nicolae.stanescu@gmail.com>
2026-05-27 19:21:41 +0300

    fix: restore Lexical richText editor and revert broken build script

---
+++ src/app/(payload)/importMap.js
@@ -0,0 +1,70 @@
+import { RscEntryLexicalCell as RscEntryLexicalCell_44fe37237e0ebf4470c9990d8cb7b07e } from '@payloadcms/richtext-lexical/rsc'
+import { RscEntryLexicalField as RscEntryLexicalField_44fe37237e0ebf4470c9990d8cb7b07e } from '@payloadcms/richtext-lexical/rsc'
+import { LexicalDiffComponent as LexicalDiffComponent_44fe37237e0ebf4470c9990d8cb7b07e } from '@payloadcms/richtext-lexical/rsc'
+import { InlineToolbarFeatureClient as InlineToolbarFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { FixedToolbarFeatureClient as FixedToolbarFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { HeadingFeatureClient as HeadingFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { ParagraphFeatureClient as ParagraphFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { UnderlineFeatureClient as UnderlineFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { BoldFeatureClient as BoldFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { ItalicFeatureClient as ItalicFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { LinkFeatureClient as LinkFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { HorizontalRuleFeatureClient as HorizontalRuleFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { CollectionCards as CollectionCards_0 } from '@payloadcms/next/rsc'
+import { FolderField as FolderField_0 } from '@payloadcms/next/rsc'
+import { FolderTableCell as FolderTableCell_0 } from '@payloadcms/next/rsc'
+import { DocumentHeader as DocumentHeader_0 } from '@payloadcms/next/rsc'
+import { Logo as Logo_0 } from '@payloadcms/next/rsc'
+import { DefaultNav as DefaultNav_0 } from '@payloadcms/next/rsc'
+import { DefaultNavClient as DefaultNavClient_0 } from '@payloadcms/next/client'
+import { NavHamburger as NavHamburger_0 } from '@payloadcms/next/client'
+import { NavWrapper as NavWrapper_0 } from '@payloadcms/next/client'
+import { FolderTypeField as FolderTypeField_0 } from '@payloadcms/next/client'
+import { SlugField as SlugField_0 } from '@payloadcms/next/client'
+import { QueryPresetsAccessCell as QueryPresetsAccessCell_0 } from '@payloadcms/next/client'
+import { QueryPresetsColumnField as QueryPresetsColumnField_0 } from '@payloadcms/next/client'
+import { QueryPresetsColumnsCell as QueryPresetsColumnsCell_0 } from '@payloadcms/next/client'
+import { QueryPresetsGroupByCell as QueryPresetsGroupByCell_0 } from '@payloadcms/next/client'
+import { QueryPresetsGroupByField as QueryPresetsGroupByField_0 } from '@payloadcms/next/client'
+import { QueryPresetsWhereCell as QueryPresetsWhereCell_0 } from '@payloadcms/next/client'
+import { QueryPresetsWhereField as QueryPresetsWhereField_0 } from '@payloadcms/next/client'
+import { VercelBlobClientUploadHandler as VercelBlobClientUploadHandler_0 } from '@payloadcms/storage-vercel-blob/client'
+import { default as CustomLogo } from '../../components/admin/Logo'
+import { default as CustomIcon } from '../../components/admin/Icon'
+
+/** @type {import('payload').ImportMap} */
+export const importMap = {
+  "@payloadcms/richtext-lexical/rsc#RscEntryLexicalCell": RscEntryLexicalCell_44fe37237e0ebf4470c9990d8cb7b07e,
+  "@payloadcms/richtext-lexical/rsc#RscEntryLexicalField": RscEntryLexicalField_44fe37237e0ebf4470c9990d8cb7b07e,
+  "@payloadcms/richtext-lexical/rsc#LexicalDiffComponent": LexicalDiffComponent_44fe37237e0ebf4470c9990d8cb7b07e,
+  "@payloadcms/richtext-lexical/client#InlineToolbarFeatureClient": InlineToolbarFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#FixedToolbarFeatureClient": FixedToolbarFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#HeadingFeatureClient": HeadingFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#ParagraphFeatureClient": ParagraphFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#UnderlineFeatureClient": UnderlineFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#BoldFeatureClient": BoldFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#ItalicFeatureClient": ItalicFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#LinkFeatureClient": LinkFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#HorizontalRuleFeatureClient": HorizontalRuleFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/next/rsc#CollectionCards": CollectionCards_0,
+  "@payloadcms/next/rsc#FolderField": FolderField_0,
+  "@payloadcms/next/rsc#FolderTableCell": FolderTableCell_0,
+  "@payloadcms/next/rsc#DocumentHeader": DocumentHeader_0,
+  "@payloadcms/next/rsc#Logo": Logo_0,
+  "@payloadcms/next/rsc#DefaultNav": DefaultNav_0,
+  "@payloadcms/next/client#DefaultNavClient": DefaultNavClient_0,
+  "@payloadcms/next/client#NavHamburger": NavHamburger_0,
+  "@payloadcms/next/client#NavWrapper": NavWrapper_0,
+  "@payloadcms/next/client#FolderTypeField": FolderTypeField_0,
+  "@payloadcms/next/client#SlugField": SlugField_0,
+  "@payloadcms/next/client#QueryPresetsAccessCell": QueryPresetsAccessCell_0,
+  "@payloadcms/next/client#QueryPresetsColumnField": QueryPresetsColumnField_0,
+  "@payloadcms/next/client#QueryPresetsColumnsCell": QueryPresetsColumnsCell_0,
+  "@payloadcms/next/client#QueryPresetsGroupByCell": QueryPresetsGroupByCell_0,
+  "@payloadcms/next/client#QueryPresetsGroupByField": QueryPresetsGroupByField_0,
+  "@payloadcms/next/client#QueryPresetsWhereCell": QueryPresetsWhereCell_0,
+  "@payloadcms/next/client#QueryPresetsWhereField": QueryPresetsWhereField_0,
+  "@payloadcms/storage-vercel-blob/client#VercelBlobClientUploadHandler": VercelBlobClientUploadHandler_0,
+  "@/components/admin/Logo#default": CustomLogo,
+  "@/components/admin/Icon#default": CustomIcon,
+}
